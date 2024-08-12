1d1a9ba475acc0b882df7abe1627a454cbec768d
DauKiet <kietdnhe180282@gmail.com>
2024-08-12 21:22:29 +0700

    Connect DB with KietDN branch

--- src/java/dto/DBContext.java
+++ src/java/dto/DBContext.java
@@ -10,17 +10,10 @@ public class DBContext {
     protected Connection connection;
        public DBContext() {
         try {
-<<<<<<< HEAD
             // Edit URL, username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1434;databaseName=HolaLearn";
             String username = "sa";
             String password = "123456";
-=======
-            // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=HolaLearn1";
-            String username = "sa";
-            String password = "trung2003";
->>>>>>> origin/main
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection established successfully.");
